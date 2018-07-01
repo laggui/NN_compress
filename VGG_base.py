9d891f55ec43a9b4a214d383a823e3d6941066d3
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2018-07-01 17:11:24 -0400

    Added depthwise separable conv VGG architecture

--- VGG_base.py
+++ VGG_base.py
@@ -13,7 +13,7 @@
 # and jumping by 2 pixels to do the next calculation (no overlap), so the spatial resolution is divided by 
 # 2 in each pooling layer
 # 
-# Thus, by starting with RGB images of size 224x224 pixels (as the paper specifiesd), with 5 MaxPool layers we get: 
+# Thus, by starting with RGB images of size 224x224 pixels (as the paper specified), with 5 MaxPool layers we get: 
 # 224/(2^5) = 7 
 # Input of first fully connected layer = output of last conv layer after MaxPool = 7x7x512
 # 
@@ -62,21 +62,35 @@ class VGG(nn.Module):
         '''
         For portability, if other configurations of the network 
         need to be defined (e.g., A, B, C or E from the VGG paper)
-        ''' 
+        
+        D: Standard D config as per the VGG paper
+        D-DSM: Compact D config with depthwise separable (DS) convolutions with maxpool layers (as per the classic VGGNet)
+        D-DS: Compact D config with depthwise separable (DS) convolutions, using conv stride=2 instead of maxpool layers to reduce spatial size
+        '''
         cfg = {
 				'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
-				'COMPACT': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'] # Compact model with depthwise separable convolutions
+				'D-DSM': [64, (64, 1), 'M', (128, 1), (128, 1), 'M', (256, 1), (256, 1), (256, 1), 'M', (512, 1), (512, 1), (512, 1), 'M', (512, 1), (512, 1), (512, 1), 'M'] 
+                'D-DS': [64, (64, 2), (128, 2), (128, 1), (256, 2), (256, 1), (256, 1), (512, 2), (512, 1), (512, 1), (512, 2), (512, 1), (512, 1)]
 		}
         
         in_channels = 3 # RGB images
         layers = []
         
         for x in cfg[vgg_cfg]:
-            if x == 'M': # MaxPool: 2x2 kernel, 2px stride
+            if x == 'M':
                 layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-            else: # Convolution: 3x3 filters, 1px stride, 1px padding            
-                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1), nn.ReLU(inplace=True)]
-                in_channels = x # Next conv input is the size of current output
+            else:
+                if isinstance(x, int):
+                    layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1), nn.BatchNorm2d(x), nn.ReLU(inplace=True)]
+                    in_channels = x # Next input is the size of current output
+                else: # Depthwise separable
+                    layers += [nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=x[1], padding=1, groups=in_channels, bias=False),
+                               nn.BatchNorm2d(in_channels),
+                               nn.ReLU(inplace=True), 
+                               nn.Conv2d(in_channels, x[0], kernel_size=1, padding=0, bias=False), 
+                               nn.BatchNorm2d(x[0]), 
+                               nn.ReLU(inplace=True)]
+                    in_channels = x[0] # Next input is the size of current output                    
         
         return nn.Sequential(*layers)
     
