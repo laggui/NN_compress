76e579af44f7234d9b8649472504bdb1e4bb22fe
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2018-07-02 09:43:20 -0400

    Added depthwise separable VGG16 training

--- train.py
+++ train.py
@@ -76,8 +76,8 @@ transform_train = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-train_loader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+train_loader = DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 
 # Model
 print('==> Building model...')
